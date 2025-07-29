c4472beec8a25f6b238aca4b90737bcccf441d82
Abinashaayan <abinash.sinha@aayaninfotech.com>
2025-07-29 13:06:42 +0530

    update ip url

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://98.85.246.54:3512/api";
+export const API_BASE_URL = "http://54.205.149.77:3512/api";
 // export const API_BASE_URL = "http://localhost:3512/api";
