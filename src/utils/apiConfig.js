c6f34993be6bdaf944ecbd925376981d4d982ab2
Abinashaayan <abinash.sinha@aayaninfotech.com>
2025-07-31 13:13:58 +0530

    update port

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = "http://54.205.149.77:3512/api";
+// export const API_BASE_URL = "http://54.205.149.77:3512/api";
+export const API_BASE_URL = "http://54.205.149.77:3515/api";
 // export const API_BASE_URL = "http://localhost:3512/api";
